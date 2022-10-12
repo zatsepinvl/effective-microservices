12ecbc72374b1b4d09c40cfabff4d73b840e4c27
Vladimir Zatsepin <zatsepin.vl@gmail.com>
2022-10-12 22:55:28 +0200

    Initial structure

---
+++ service-template/src/main/java/org/effective/microservices/template/TemplateMain.java
@@ -0,0 +1,7 @@
+package org.effective.microservices.template;
+
+public class TemplateMain {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ service-template/src/test/java/com/effective/microservices/template/TemplateTest.java
@@ -0,0 +1,11 @@
+package com.effective.microservices.template;
+
+import org.junit.jupiter.api.Test;
+
+public class TemplateTest {
+
+    @Test
+    void test() {
+
+    }
+}
